f344d132072ae61225c4203d177b7204051b0afe
Jon Fackrell <jon.fackrell@gmail.com>
2018-09-14 11:41:22 -0600

    no message

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 Route::view('/', 'home');
 
-/*Route::get('/3d', function(){
+Route::get('/3d', function(){
     return redirect()->to('https://maclab.byui.edu/3d/');
-});*/
+});
 
 Route::get('/admin', 'AdminController@home')->name('admin');
