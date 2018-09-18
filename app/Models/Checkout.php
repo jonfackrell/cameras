2d34fe025a639d09b70d2345acfd2faeeab5f0cd
Jon Fackrell <jon.fackrell@gmail.com>
2018-09-18 14:24:59 -0600

    no message

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Checkout extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'checked_out_at', 'checked_in_at', 'checkout_note', 'checkin_note',
+    ];
 }
