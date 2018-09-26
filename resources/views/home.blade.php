934a9bcb261d07055cc8ed0d97c69ef4b5231016
England, Anthony <eng15004@byui.edu>
2018-09-26 15:00:43 -0600

    redesigned homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,9 @@
 @section('nav')
     <div class="container-fluid"  id="button-container">
         <div class="container">
-            <div class="row justify-content-around">
+            <div class="row justify-content-center">
                 
-                    <a href="{{ route('equipment.home') }}" class="col-xs-2 btn btn-light">
+                    <a href="{{ route('equipment.home') }}" class="col-3 btn btn-light">
                         <div class="clip-wrap">
                             <div class="clip-each border-style-thin">
                                 <img src="/img/equipment.png" class="nav-icon">
@@ -25,7 +25,7 @@
                     </a>
                 
                 
-                    <a href="{{ route('3d.home') }}" class="col-xs-2 btn btn-light">
+                    <a href="{{ route('3d.home') }}" class="col-3 btn btn-light">
                         <div class="clip-wrap">
                             <div class="clip-each border-style-thin">
                                 <img src="/img/3dPrinting.png" class="nav-icon">
@@ -37,7 +37,7 @@
                     </a>
                 
                 
-                    <a href="{{ route('maclab-policies') }}" class="col-xs-2 btn btn-light">
+                    <a href="{{ route('maclab-policies') }}" class="col-3 btn btn-light">
                         <div class="clip-wrap">
                             <div class="clip-each border-style-thin">
                                 <img src="/img/policies.png" class="nav-icon">
@@ -55,27 +55,45 @@
 
 @section('content')
 
+    <div class="clearfix">&nbsp;</div>
+    <div id="hours-container" class="container" style="text-align: center;">
+        <a href="https://byui.libcal.com/hours/">
+            <h2 id="hours-button">Today's Hours: 7am - 11:30pm</h2>
+        </a>
+    </div>
+    <div class="clearfix">&nbsp;</div>
 
     <h2>OPEN COMPUTER LAB</h2>
-    <p>The purpose of David O. McKay Library Mac Lab is to provide students with a creative space to help them in their academic pursuits by providing access to software, hardware, and trained lab assistants.</p>
-    <p>The Mac Lab provides access to several programs, both commercial and free, that facilitate the creation of digital media, 3D models, and software applications. Some of which are included below. This list is constantly changing to adapt to faculty requirements.</p>
-    <p><strong>Adobe Creative Cloud Applications:</strong> Illustrator, Photoshop, InDesign, Premiere Pro, Acrobat, XD, and many others.</p>
-    <p><strong>3D Printing:</strong> Cura, Maya, Inventor</p>
-    <p><strong>Microsoft Office:</strong> Word, PowerPoint, Excel</p>
-    <p><strong>Software Development:</strong> XCode, BBEdit, RStudio, NetBeans, Cyberduck</p>          
-    
-    <h2>HOURS</h2>
-    <ul class="hours">
-        <li><strong>Monday-Thursday: </strong> 7am - 11:30pm</li>
-        <li><strong>Friday:</strong> 7am - 9pm</li>
-        <li><strong>Saturday:</strong> 9am - 9pm</li>
-        <li><strong>Sunday: </strong>Closed</li>
-        <li>The Mac Lab is closed on Tuesday for Devotional from 2pm - 3pm.</li>
+    <p>The purpose of David O. McKay Library Mac Lab is to provide students with a creative space to help them in their academic pursuits by providing access to software, hardware, and trained lab assistants. The Mac Lab provides access to several programs, both commercial and free, that facilitate the creation of digital media, 3D models, and software applications. Some of which are included below. This list is constantly changing to adapt to faculty requirements.</p>
+    <ul>
+        <li><h3>Adobe Creative Cloud Applications:</h3>Illustrator, Photoshop, InDesign, Premiere Pro, Acrobat, XD, and many others.</li>
+        <li><h3>3D Printing:</h3>Cura, Maya, Inventor</li>
+        <li><h3>Microsoft Office:</h3>Word, PowerPoint, Excel</li>
+        <li><h3>Software Development:</h3>XCode, BBEdit, RStudio, NetBeans, Cyberduck</li>
     </ul>
-    <p>See the <a class="bluelink" href="http://library.byui.edu/library-information/library-hours.htm">Library Schedule</a> for Holiday Hours.</p>
-
-
-
+         
+    
 @endsection
 
+@push('footer-scripts')
+
+<script type="text/javascript">
+    var t=$("#hours-button");
+    $.ajax({
+        url:"https://api3.libcal.com/api_hours_today.php?iid=4251&lid=0&format=json&systemTime=0", 
+        type:"GET", 
+        dataType:"jsonp", 
+        timeout:3e3, 
+        success:function(n){ 
+            var e=n.locations[0]; 
+            if(1==e.times.currently_open?t.html("Today's Hours: "+e.rendered):t.html("Closed"),n.hasOwnProperty("NOTICE")){
+                var o='<br /><span style="font-size: 11px;">'+n.NOTICE+"</span>";
+                t.append(o)
+            }
+            t.parents("div#hours-container").show()
+        },
+        error:function(n,e,o){t.parents("div#hours-container").hide()}});
+</script>
+
+@endpush
 
