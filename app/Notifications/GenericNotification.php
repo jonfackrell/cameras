380bf2aaacfa78703ac2c3b30128c54bbb65ad2e
Jon Fackrell <jon.fackrell@gmail.com>
2019-01-04 14:23:11 -0700

    Made generic notifications queuable

--- app/Notifications/GenericNotification.php
+++ app/Notifications/GenericNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class GenericNotification extends Notification
+class GenericNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
